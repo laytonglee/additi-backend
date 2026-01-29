3f1c1759ad9dd18c9253dc9aa3bee8b9ac4d09b2
laytonglee <laytongly@gmail.com>
2026-01-29 23:02:43 +0700

    First Commit

---
+++ src/test/java/groupproject/additibackend/AdditiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package groupproject.additibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
