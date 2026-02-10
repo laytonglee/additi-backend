7424d983fdecea1ac3cc35c10b10e6000c0cb48c
Vanreuth <hengvanreuth.17@gmail.com>
2026-02-10 16:13:08 +0700

    update product service

--- src/main/java/groupproject/additibackend/service/ProductService.java
+++ src/main/java/groupproject/additibackend/service/ProductService.java
@@ -1,7 +1,9 @@
 package groupproject.additibackend.service;
 
 import groupproject.additibackend.request.ProductCreateRequest;
+import groupproject.additibackend.request.ProductUpdateRequest;
 import groupproject.additibackend.response.PageResponse;
+import groupproject.additibackend.response.ProductDetailResponse;
 import groupproject.additibackend.response.ProductResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,10 +12,45 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface ProductService {
 
-    ProductResponse createProduct(ProductCreateRequest request);
+    ProductResponse createProduct(
+            ProductCreateRequest request,
+            Map<Integer ,List<MultipartFile>> varaintImages) throws IOException;
+
+
+    ProductResponse updateProduct(Long productId, ProductUpdateRequest request);
+
+    ProductResponse updateProductWithImages(
+            Long id,
+            ProductUpdateRequest request,
+            Map<Integer ,List<MultipartFile>> varaintImages
+    ) throws IOException;
+
+//    PageResponse<ProductResponse> getAllProducts(
+//            String search,
+//            String categorySlug,
+//            BigDecimal minPrice,
+//            BigDecimal maxPrice,
+//            LocalDate startDate,
+//            LocalDate endDate,
+//            String size,
+//            String color,
+//            int page,
+//            int limit,
+//            String sortBy,
+//            String sortDir
+//    );
+
+
+    ProductResponse uploadImage(Long productId, Long variantId, List<MultipartFile> files) throws IOException;
+
+    ProductDetailResponse getProductById(Long id);
+
+    void deleteProduct(Long id);
+
 
     PageResponse<ProductResponse> getAllProducts(
             String search,
@@ -22,14 +59,7 @@ public interface ProductService {
             BigDecimal maxPrice,
             LocalDate startDate,
             LocalDate endDate,
+            String size,
+            String color,
             Pageable pageable);
-
-    ProductResponse uploadImage(Long productId, Long variantId, List<MultipartFile> files) throws IOException;
-
-    ProductResponse getProductById(Long id);
-
-    void deleteProduct(Long id);
-
-
-
 }
